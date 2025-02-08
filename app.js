2488938bfbf204f74f64ed9f0b5fcb1cce546213
thaistrok <thaistrok@gmail.com>
2025-02-08 20:39:43 +0300

    arrays-lab

--- app.js
+++ app.js
@@ -288,3 +288,34 @@ const numArrays = [
  
  console.log('Exercise 15 result:\n', total);
  
+
+ /*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(mark) {
+   if (mark >= 90) {
+      return'A';
+   }else if (mark >= 80){
+      return'B';
+   }else if(mark >= 70){
+      return'C';
+   }else if(mark >= 60){
+      return'D';
+   }else {
+      return'F';      
+   }
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
